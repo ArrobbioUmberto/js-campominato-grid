89ca716f47fbc46d9dce26097064e1b231f2659d
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-09 18:12:18 +0100

    FEAT font size fix

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ btnElement.addEventListener('click', function(){
     appendHTML.innerHTML = ''
     for (let i = 0; i < tabellaArea; i++){
         console.log(i+1)
-        const createDiv = `<div class="d-flex flex-row flex-nowrap p-2 border justify-content-center align-items-center cella" style="width: calc(100%/${numeroCelle});aspect-ratio:1">${i+1}</div>`
+        const createDiv = `<div class="d-flex flex-row flex-nowrap p-2 border justify-content-center align-items-center cella fs-2" style="width: calc(100%/${numeroCelle});aspect-ratio:1">${i+1}</div>`
         appendHTML.innerHTML += createDiv    
     }
     console.log(appendHTML)
