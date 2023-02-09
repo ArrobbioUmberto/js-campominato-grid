7ddb9f20299e99d4d2e882742f44286ea9c05356
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-09 17:55:01 +0100

    FEAT number click in console

--- js/app.js
+++ js/app.js
@@ -13,10 +13,25 @@ console.log(btnElement)
 btnElement.addEventListener('click', function(){
     for (let i = 0; i < tabellaArea; i++){
         console.log(i+1)
-        const createDiv = `<div class="d-flex flex-row flex-nowrap p-2 border justify-content-center align-items-center" style="width: calc(100%/${numeroCelle});aspect-ratio:1">${i+1}</div>`
-        appendHTML.innerHTML += createDiv
+        const createDiv = `<div class="d-flex flex-row flex-nowrap p-2 border justify-content-center align-items-center cella" style="width: calc(100%/${numeroCelle});aspect-ratio:1">${i+1}</div>`
+        appendHTML.innerHTML += createDiv    
     }
     console.log(appendHTML)
+    const celleElements = document.querySelectorAll('.cella')
+      console.log(celleElements)
+
+    for (let i = 0; i < celleElements.length; i++) {
+	const cella = celleElements[i]
+
+	cella.addEventListener('click', function () {
+		console.log(i + 1)
+	})
+}
 })
+
+
+
+
+
 
 
