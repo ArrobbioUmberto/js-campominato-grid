491838f3f113660a82574dae8b6885a4c4ade10e
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-09 18:04:16 +0100

    FIX double table with paly button

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ const appendHTML = document.querySelector('.row')
 const btnElement = document.querySelector('.btn')
 console.log(btnElement)
 btnElement.addEventListener('click', function(){
+    appendHTML.innerHTML = ''
     for (let i = 0; i < tabellaArea; i++){
         console.log(i+1)
         const createDiv = `<div class="d-flex flex-row flex-nowrap p-2 border justify-content-center align-items-center cella" style="width: calc(100%/${numeroCelle});aspect-ratio:1">${i+1}</div>`
