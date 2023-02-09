aaa2ab01201f8ea163276ad01adbc3b5e96cd94c
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-09 16:02:19 +0100

    FEAT table with number

--- js/app.js
+++ js/app.js
@@ -1,2 +1,18 @@
 console.log('ciao')
 
+
+const numeroCelle = 10
+console.log(numeroCelle)
+const tabellaArea = numeroCelle**2 
+console.log(tabellaArea)
+
+const appendHTML = document.querySelector('.row')
+
+
+for (let i = 0; i < tabellaArea; i++){
+    console.log(i+1)
+    const createDiv = `<div class="d-flex flex-row flex-nowrap p-2 border justify-content-center align-items-center" style="width: calc(100%/${numeroCelle});aspect-ratio:1">${i+1}</div>`
+    appendHTML.innerHTML += createDiv
+}
+console.log(appendHTML)
+
