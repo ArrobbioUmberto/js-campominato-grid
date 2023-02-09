2ae15d8eb6068de3fb18546a813e3339d1154128
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-09 17:05:33 +0100

    FEAT btn paly set

--- js/app.js
+++ js/app.js
@@ -8,11 +8,15 @@ console.log(tabellaArea)
 
 const appendHTML = document.querySelector('.row')
 
+const btnElement = document.querySelector('.btn')
+console.log(btnElement)
+btnElement.addEventListener('click', function(){
+    for (let i = 0; i < tabellaArea; i++){
+        console.log(i+1)
+        const createDiv = `<div class="d-flex flex-row flex-nowrap p-2 border justify-content-center align-items-center" style="width: calc(100%/${numeroCelle});aspect-ratio:1">${i+1}</div>`
+        appendHTML.innerHTML += createDiv
+    }
+    console.log(appendHTML)
+})
 
-for (let i = 0; i < tabellaArea; i++){
-    console.log(i+1)
-    const createDiv = `<div class="d-flex flex-row flex-nowrap p-2 border justify-content-center align-items-center" style="width: calc(100%/${numeroCelle});aspect-ratio:1">${i+1}</div>`
-    appendHTML.innerHTML += createDiv
-}
-console.log(appendHTML)
 
